a8ccda6250d83c025fdc1624c9ce448826653f5e
Adriel Medeiros <adriel.souza397@gmail.com>
2021-06-26 22:12:52 -0300

    renaming functions

--- script.js
+++ script.js
@@ -7,17 +7,17 @@ snake[0] ={
   y: 8*box
 }
 
-function criarBG() {
+function createBG() {
   context.fillStyle = "lightgreen";
   context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
-function criarCobrinha(){
+function createSnake(){
   for(i=0; i<snake.length; i++){
     context.fillStyle = "green";
     context.fillRect(snake[i].x, snake[i].y, box, box);
   }
 }
 
-criarBG();
-criarCobrinha();
+createBG();
+createSnake();
