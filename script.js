ad9ada721d482130db71f9ec7f77b48c58ff9ad3
Adriel Medeiros <adriel.souza397@gmail.com>
2021-06-27 10:36:17 -0300

    implementing basic styling

--- script.js
+++ script.js
@@ -63,8 +63,8 @@ function startGame() {
     snake[0].y = 16 * box;
   }
 
-  for(i=1; i<snake.length; i++){
-    if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+  for (i = 1; i < snake.length; i++) {
+    if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
       clearInterval[game];
       alert("Game Over !!!")
     }
@@ -94,7 +94,7 @@ function startGame() {
     snake.pop();
   } else {
     foodLocation.x = Math.floor(Math.random() * 15 + 1) * box,
-    foodLocation.y = Math.floor(Math.random() * 15 + 1) * box
+      foodLocation.y = Math.floor(Math.random() * 15 + 1) * box
   }
 
   let newHead = {
