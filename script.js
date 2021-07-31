0c776b9afc3f5accfc7b09179dcf2aaf15f95f1d
Adriel Medeiros <adriel.souza397@gmail.com>
2021-07-31 11:50:24 -0300

    Moving the snake's head

--- script.js
+++ script.js
@@ -101,18 +101,23 @@ function update(event) {
 
 function startGame() {
 
-  if (snake[0].x > 15 * box && direction == "right") {
-    snake[0].x = 0;
-  }
-  if (snake[0].x < 0 && direction == "left") {
-    snake[0].x = 16 * box;
-  }
-  if (snake[0].y > 15 * box && direction == "down") {
-    snake[0].y = 0;
-  }
-  if (snake[0].y < 0 && direction == "up") {
-    snake[0].y = 16 * box;
-  }
+  // if (snake[0].x > 15 * box && direction == "right") {
+  //   snake[0].x = 0;
+  // }
+  // if (snake[0].x < 0 && direction == "left") {
+  //   snake[0].x = 16 * box;
+  // }
+  // if (snake[0].y > 15 * box && direction == "down") {
+  //   snake[0].y = 0;
+  // }
+  // if (snake[0].y < 0 && direction == "up") {
+  //   snake[0].y = 16 * box;
+  // }
+
+  if (snake[0].x > 15 * box && snake[0].direction.x == 1) snake[0].x = 0;
+  if (snake[0].x < 0 && snake[0].direction.x == -1) snake[0].x = 16 * box;
+  if (snake[0].y > 15 * box && snake[0].direction.y == 1) snake[0].y = 0;
+  if (snake[0].y < 0 && snake[0].direction.y == -1) snake[0].y = 16 * box;
 
   for (i = 1; i < snake.length; i++) {
     if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
@@ -128,18 +133,23 @@ function startGame() {
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
 
-  if (direction == "right") {
-    snakeX += box;
-  }
-  if (direction == "left") {
-    snakeX -= box;
-  }
-  if (direction == "up") {
-    snakeY -= box;
-  }
-  if (direction == "down") {
-    snakeY += box;
-  }
+  // if (direction == "right") {
+  //   snakeX += box;
+  // }
+  // if (direction == "left") {
+  //   snakeX -= box;
+  // }
+  // if (direction == "up") {
+  //   snakeY -= box;
+  // }
+  // if (direction == "down") {
+  //   snakeY += box;
+  // }
+
+  if (snake[0].direction.x == 1) snakeX += box;
+  if (snake[0].direction.x == -1) snakeX -= box;
+  if (snake[0].direction.y == -1) snakeY -= box;
+  if (snake[0].direction.y == 1) snakeY += box;
 
   if (snakeX != foodLocation.x || snakeY != foodLocation.y) {
     snake.pop();
